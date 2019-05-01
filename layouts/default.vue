24425c8d1244179fbd98ee58d70b55a65da0bfec
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-05-01 14:06:20 -0700

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,24 @@
     <nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Po Rith | Rithfolio.com'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'Po Rith | Rithfolio.com', name: 'Po Rith | Rithfolio.com', content: 'Po Rith is a designer and vue.js developer from Oakland, CA.' }
+      ]
+    }
+  }
+}
+</script>
+
 <style>
 </style>
