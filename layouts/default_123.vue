f36430703bcfeb634420105f80e87783f736966c
jpoechill <poamrong@rith.com>
2022-02-27 19:12:38 -0800

    2022 update

---
+++ layouts/default_123.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mt-5">
+          <img src="/temp.png" class="w-100" alt="Down for updates.">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,188 @@
 <template>
-  <div></div>
+  <div>
+
+
+    <div class="container pt-5">
+      <div class="row pt-3 mt-4">
+        <div class="offset-md-1 col-md-3">
+          <img src="/avatar_22.jpg" class="w-100" style="border-radius: 500px;" alt="">
+        </div>
+        <div class="offset-md-1 col-md-6">
+          <p class="">
+            Greetings! <br><br> I'm Po, a frontend developer with a background in design. <br>
+          </p>
+          <p>
+            I most enjoy building front-end applications with JavaScript, utilizing Vue.js as my framework of choice. <br><br>
+            I've been practicing for a while now, and I have a great passion for what I do. 
+            If you are interested in learning more about me, or my work, please feel free to get in touch!
+            <br>
+            <!-- <span class="text-muted">
+              <small><em>
+                Hashtags: #Design, #Javascript, #Vue.js, #HTML/CSS, #Algorithms, #Git
+              </em></small>
+            </span> -->
+          </p>
+          <p class="text-right pt-2">
+            Email, Github, LinkedIn
+          </p>
+          <!-- <p style="color: #777;">
+            <em>
+              Keywords: Design, Prototyping, Javascript, Vue.js, HTML/CSS, Git
+            </em>
+          </p> -->
+
+        </div>
+        <div class="col-md-12 pt-5 mt-3">
+          <hr>
+        </div>
+      </div>
+      <div class="row pt-3 mt-4 pb-4">
+        <div class="col-md-12 text-center">
+          Recent Projects
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-6 pb-5">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-04.png" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-3 py-2">
+            <span class="font-weight-bold text-light">
+              PPCS Restore
+            </span><br>
+            <p class="mb-1">
+              PPCS Restore is a national, family-run, commercial building restoration company based in the Bay Area.
+            </p>
+            <a href="https://ppc-restore.com">Live Site</a> | <a href="https://github.com/jpoechill/ppcs">Github</a>
+          </div>          
+        </div>
+
+
+        <div class="col-md-6 pb-5">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-05.jpg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-3">
+            <span class="font-weight-bold text-light">
+              Mellana Café
+            </span><br>
+            <p class="mb-1">
+              Mellana Café is a café located in South Berkeley/Northern Oakland, offering hand-made juices, gourmet sandwiches, and coffee.
+            </p>
+            <a href="https://mellanacafe.com">Live Site</a> | 
+            <a href="https://instagram.com/mellanacafe">Instagram</a>
+          </div>
+        </div>
+
+        <!-- <div class="col-md-6 pb-4">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-05.jpg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-3 ">
+            <span class="font-weight-bold text-light">
+              Mellana Café
+            </span><br>
+            <p class="mb-1">
+              Mellana Café is a café located in South Berkeley/Northern Oakland, offering hand-made juices, gourmet sandwiches, and coffee.
+            </p>
+            <a href="https://mellanacafe.com">Live Site</a> | 
+            <a href="https://instagram.com/mellanacafe">Instagram</a>
+          </div>
+        </div>
+
+        <div class="col-md-6 pl-3">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-05.jpg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-2">
+            <span class="font-weight-bold text-light">
+              Mellana Café
+            </span><br>
+            <p class="mb-1">
+              Mellana Café is a café located in South Berkeley/Northern Oakland, offering hand-made juices, gourmet sandwiches, and coffee.
+            </p>
+            <a href="https://mellanacafe.com">Live Site</a> | 
+            <a href="https://instagram.com/mellanacafe">Instagram</a>
+          </div>
+        </div> -->
+
+                <!-- <div class="col-md-4 pb-4">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-05.jpg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-2">
+            <span class="font-weight-bold text-light">
+              Mellana Café
+            </span><br>
+            <p class="mb-1">
+              Mellana Café is a café located in South Berkeley/Northern Oakland, offering hand-made juices, gourmet sandwiches, and coffee.
+            </p>
+            <a href="https://mellanacafe.com">Homepage</a> | 
+            <a href="https://instagram.com/mellanacafe">Instagram</a>
+          </div>
+        </div>
+
+                <div class="col-md-4 pb-4">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-05.jpg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-2">
+            <span class="font-weight-bold text-light">
+              Mellana Café
+            </span><br>
+            <p class="mb-1">
+              Mellana Café is a café located in South Berkeley/Northern Oakland, offering hand-made juices, gourmet sandwiches, and coffee.
+            </p>
+            <a href="https://mellanacafe.com">Homepage</a> | 
+            <a href="https://instagram.com/mellanacafe">Instagram</a>
+          </div>
+        </div>
+
+                <div class="col-md-4 pb-4">
+          <div class="w-100 mb-1 position-relative fixed-ratio">
+            <div class="position-absolute">
+              <img src="/browser-05.jpg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="pl-2">
+            <span class="font-weight-bold text-light">
+              Mellana Café
+            </span><br>
+            <p class="mb-1">
+              Mellana Café is a café located in South Berkeley/Northern Oakland, offering hand-made juices, gourmet sandwiches, and coffee.
+            </p>
+            <a href="https://mellanacafe.com">Homepage</a> | 
+            <a href="https://instagram.com/mellanacafe">Instagram</a>
+          </div>
+        </div> -->
+
+
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-12 pb-5 text-center">
+          <hr class="pt-5 my-5">
+          I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.
+          <br>
+          – Bruce Lee
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -8,9 +191,9 @@ export default {
     return {}
   },
   created() {
-    this.$router.push({
-        path: '/working'
-    })
+    // this.$router.push({
+    //     path: '/working'
+    // })
   },
   transition: 'fade'
 }
